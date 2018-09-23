fa6a9374c0d40f0701c567a7be24655a95f0ec81
Christoph Alt <christoph.alt@dfki.de>
2018-09-23 23:46:04 +0200

    conversion of doc to brat format

--- macss_webservice/app/app_core.py
+++ macss_webservice/app/app_core.py
@@ -9,6 +9,7 @@ from macss_webservice.api_helpers.exception import UserException
 from macss_webservice.webservice_settings import CONFIG_SERVER, HOSTNAME
 from macss_webservice.api_helpers.input import required_parameter
 from macss_medical_ie.macss_medical_ie_pipeline import MedicalIEPipeline
+from macss_medical_ie.utils.document_helper import doc_to_brat
 
 
 _endpoint_route = lambda x: app_endpoints.route(URL_BASE + x, methods=['GET', 'POST'])
@@ -25,4 +26,5 @@ def _test(request):
 def _annotate(request):
     text = request.json['text']
     doc = MedicalIEPipeline.get_annotated_document(text)
-    return {'entities': doc.ents}
+    return doc_to_brat(doc)
+
